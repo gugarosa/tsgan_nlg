6843ed91355a4574bb3a7461ff09ca03f1379518
Gustavo Rosa <gth.rosa@uol.com.br>
2021-04-09 16:31:00 -0300

    Recurrent models seems to be working as intended.

---
+++ generation/recurrent_models.py
@@ -0,0 +1,51 @@
+from nalp.models.generators import (GRUGenerator, LSTMGenerator, RMCGenerator,
+                                    RNNGenerator)
+
+
+class RecurrentModel:
+    """A RecurrentModel class help users in selecting distinct recurrent architectures from the command line.
+
+    """
+
+    def __init__(self, obj):
+        """Initialization method.
+
+        Args:
+            obj (Generator): A Generator-child instance.
+
+        """
+
+        # Creates a property to hold the class itself
+        self.obj = obj
+
+
+# Defines a model dictionary constant with the possible values
+RECURRENT_MODEL = dict(
+    gru=RecurrentModel(GRUGenerator),
+    lstm=RecurrentModel(LSTMGenerator),
+    rmc=RecurrentModel(RMCGenerator),
+    rnn=RecurrentModel(RNNGenerator)
+)
+
+
+def get_recurrent_model(name):
+    """Gets a model by its identifier.
+
+    Args:
+        name (str): RecurrentModel's identifier.
+
+    Returns:
+        An instance of the RecurrentModel class.
+
+    """
+
+    # Tries to invoke the method
+    try:
+        # Returns the corresponding object
+        return RECURRENT_MODEL[name]
+
+    # If object is not found
+    except:
+        # Raises a RuntimeError
+        raise RuntimeError(
+            f'Recurrent-based model {name} has not been specified yet.')
---
+++ recurrent_model_training.py
@@ -0,0 +1,128 @@
+import argparse
+
+import tensorflow as tf
+
+import generation.recurrent_models as r
+import utils.loader as l
+from nalp.corpus import SentenceCorpus
+from nalp.datasets import LanguageModelingDataset
+from nalp.encoders import IntegerEncoder
+from nalp.models.generators import LSTMGenerator
+
+
+def get_arguments():
+    """Gets arguments from the command line.
+
+    Returns:
+        A parser with the input arguments.
+
+    """
+
+    parser = argparse.ArgumentParser(usage='Trains recurrent-based models over NLG tasks.')
+
+    parser.add_argument('model', help='Type of model', choices=['gru', 'lstm', 'rmc', 'rnn'])
+
+    parser.add_argument('dataset', help='Dataset', choices=['amazon_customer_reviews', 'coco_image_captions',
+                                                            'google_one_billion_words', 'wmt_emnlp17_news'])
+
+    parser.add_argument('-train_split', help='Percentage of the training set', type=float, default=0.8)
+
+    parser.add_argument('-val_split', help='Percentage of the validation set', type=float, default=0.1)
+
+    parser.add_argument('-test_split', help='Percentage of the testing set', type=float, default=0.1)
+
+    parser.add_argument('-embedding_size', help='Number of embedding units', type=int, default=256)
+
+    parser.add_argument('-hidden_size', help='Number of hidden units', type=int, default=512)
+
+    parser.add_argument('-n_slots', help='Number of RMC slots', type=int, default=5)
+
+    parser.add_argument('-n_heads', help='Number of RMC heads', type=int, default=5)
+
+    parser.add_argument('-head_size', help='Size of RMC head', type=int, default=25)
+
+    parser.add_argument('-n_blocks', help='Number of RMC blocks', type=int, default=1)
+
+    parser.add_argument('-n_layers', help='Number of RMC layers', type=int, default=3)
+
+    parser.add_argument('-batch_size', help='Size of batches', type=int, default=4)
+
+    parser.add_argument('-lr', help='Learning rate', type=float, default=0.001)
+
+    parser.add_argument('-epochs', help='Amount of training epochs', type=int, default=10)
+
+    parser.add_argument('-seed', help='Seed identifier', type=int, default=0)
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    # Gathers the input parsed arguments
+    args = get_arguments()
+
+    # Common-based arguments
+    dataset = args.dataset
+    train_split = args.train_split
+    val_split = args.val_split
+    test_split = args.test_split
+    seed = args.seed
+
+    # Model-based arguments
+    model_name = args.model
+    model_obj = r.get_recurrent_model(model_name).obj
+    embedding_size = args.embedding_size
+    hidden_size = args.hidden_size
+    batch_size = args.batch_size
+    lr = args.lr
+    epochs = args.epochs
+
+    # RMC-based arguments
+    n_slots = args.n_slots
+    n_heads = args.n_heads
+    head_size = args.head_size
+    n_blocks = args.n_blocks
+    n_layers = args.n_layers
+
+    # Loads and tokenizes the data
+    data = l.load_data(dataset)
+    tokens = l.tokenize_data(data)
+
+    # Creates the sentence-based corpus
+    corpus = SentenceCorpus(tokens=tokens)
+
+    # Initializes, learns the encoder and encodes the data
+    encoder = IntegerEncoder()
+    encoder.learn(corpus.vocab_index, corpus.index_vocab)
+    encoded_tokens = encoder.encode(tokens)
+
+    # Splits the tokens
+    enc_train, enc_val, _ = l.split_data(encoded_tokens, train_split, val_split, test_split, seed)
+
+    # Creates Language Modeling datasets
+    train = LanguageModelingDataset(enc_train, batch_size=batch_size)
+    val = LanguageModelingDataset(enc_val, batch_size=batch_size)
+
+    # Checks if supplied model is an RMC
+    if model_name == 'rmc':
+        # Instantiates the model
+        model = model_obj(encoder=encoder, vocab_size=corpus.vocab_size, embedding_size=embedding_size,
+                          n_slots=n_slots, n_heads=n_heads, head_size=head_size,
+                          n_blocks=n_blocks, n_layers=n_layers)
+
+    # If is something else
+    else:
+        # Instantiates the model
+        model = model_obj(encoder=encoder, vocab_size=corpus.vocab_size,
+                          embedding_size=embedding_size, hidden_size=hidden_size)
+
+    # Builds and compiles the model
+    model.build((batch_size, None))
+    model.compile(optimizer=tf.optimizers.Adam(learning_rate=lr),
+                  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=[tf.metrics.SparseCategoricalAccuracy(name='accuracy')])
+
+    # Fits the model
+    model.fit(train.batches, epochs=epochs, validation_data=val.batches)
+
+    # Saves model weights
+    model.save_weights(f'outputs/{model_name}', save_format='tf')
