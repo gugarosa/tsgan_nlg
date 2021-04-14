964438498261fd1a131cda2e1d92f0a5c2504150
Gustavo Rosa <gth.rosa@uol.com.br>
2021-04-14 12:23:41 -0300

    Adding script to generate language with TSGAN.

---
+++ scripts/tsgan_language_generation.py
@@ -0,0 +1,68 @@
+# Caveat to allow running from inside or outside scripts package
+import os
+import sys
+
+sys.path.append(os.path.abspath('./src'))
+sys.path.append(os.path.abspath('../src'))
+
+import csv
+
+import utils.pickler as p
+from core import TSGANContrastive, TSGANTriplet
+
+
+if __name__ == '__main__':
+    # Common-based arguments
+    n_tokens = 3
+    temp = 0.5
+    top_k = 10
+    top_p = 0.9
+
+    # Model-based arguments
+    model_name = 'tsgan'
+
+    # Loads pre-pickled objects
+    corpus = p.load_from_file(f'outputs/{model_name}_corpus.pkl')
+    encoder = p.load_from_file(f'outputs/{model_name}_encoder.pkl')
+    enc_test = p.load_from_file(f'outputs/{model_name}_enc_test.pkl')
+
+    model = TSGANContrastive(encoder=encoder, vocab_size=corpus.vocab_size,
+                             embedding_size=64, hidden_size=128, temperature=0.5,
+                             n_pairs=25)
+
+    # Loads weights and builds model
+    model.load_weights(f'outputs/{model_name}').expect_partial()
+    model.G.build((1, None))
+
+    # Instantiates lists for the outputs
+    tokens, greedy_tokens, temp_tokens, top_tokens = [], [], [], []
+
+    # Iterates over every sentence in testing set
+    for token in enc_test:
+        # Decodes the token back to words
+        decoded_token = encoder.decode(token)
+
+        # Gathers `n` tokens as the starting token
+        start_token = decoded_token[:n_tokens]
+
+        # Generates with three distinct strategies
+        greedy_token = model.G.generate_greedy_search(start=decoded_token, max_length=len(decoded_token))
+        temp_token = model.G.generate_temperature_sampling(start=decoded_token, max_length=len(decoded_token),
+                                                           temperature=temp)
+        top_token = model.G.generate_top_sampling(start=decoded_token, max_length=len(decoded_token),
+                                                  k=top_k, p=top_p)
+
+        # Appends the outputs to lists
+        tokens.append(' '.join(decoded_token))
+        greedy_tokens.append(' '.join(start_token + greedy_token))
+        temp_tokens.append(' '.join(start_token + temp_token))
+        top_tokens.append(' '.join(start_token + top_token))
+
+    # Opens an output .csv file
+    with open(f'outputs/{model_name}.csv', 'w') as f:
+        # Creates the .csv writer
+        writer = csv.writer(f)
+
+        # Dumps the data
+        writer.writerow(['reference', 'greedy_search', 'temperature_sampling', 'top_sampling'])
+        writer.writerows(zip(tokens, greedy_tokens, temp_tokens, top_tokens))
--- scripts/tsgan_model_learning.py
+++ scripts/tsgan_model_learning.py
@@ -11,6 +11,7 @@ from nalp.datasets import LanguageModelingDataset
 from nalp.encoders import IntegerEncoder
 
 import utils.loader as l
+import utils.pickler as p
 from core import TSGANContrastive, TSGANTriplet
 
 if __name__ == '__main__':
@@ -36,7 +37,7 @@ if __name__ == '__main__':
     #
     model = TSGANContrastive(encoder=encoder, vocab_size=corpus.vocab_size,
                              embedding_size=64, hidden_size=128, temperature=0.5,
-                             n_pairs=2)
+                             n_pairs=25)
 
     # model = TSGANTriplet(encoder=encoder, vocab_size=corpus.vocab_size,
     #                      embedding_size=64, hidden_size=128)
@@ -48,7 +49,13 @@ if __name__ == '__main__':
                 g_optimizer=tf.optimizers.Adam(learning_rate=0.001))
 
     #
-    model.pre_fit(train.batches, g_epochs=1, d_epochs=1)
+    model.pre_fit(train.batches, g_epochs=10, d_epochs=10)
 
     #
     model.fit(train.batches, epochs=10, d_epochs=5)
+
+    # Saves model and objects to files
+    output_path = 'outputs/tsgan'
+    model.save_weights(output_path, save_format='tf')
+    p.save_to_file(output_path, train_history=model.history,
+                   corpus=corpus, encoder=encoder, enc_test=enc_test)
