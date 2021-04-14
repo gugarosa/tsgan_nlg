bf1ef575aff4708759415a9232557220dbf31007
Gustavo Rosa <gth.rosa@uol.com.br>
2021-04-14 10:43:46 -0300

    Reworking the project structure as scripts might increase in size.

--- scripts/adversarial_language_generation.py
+++ scripts/adversarial_language_generation.py
@@ -1,3 +1,10 @@
+# Caveat to allow running from inside or outside scripts package
+import os
+import sys
+
+sys.path.append(os.path.abspath('./src'))
+sys.path.append(os.path.abspath('../src'))
+
 import argparse
 import csv
 
---
+++ scripts/tsgan_model_learning.py
@@ -0,0 +1,50 @@
+# Caveat to allow running from inside or outside scripts package
+import os
+import sys
+
+sys.path.append(os.path.abspath('./src'))
+sys.path.append(os.path.abspath('../src'))
+
+import tensorflow as tf
+from nalp.corpus import SentenceCorpus
+from nalp.datasets import LanguageModelingDataset
+from nalp.encoders import IntegerEncoder
+
+import utils.loader as l
+from core import TSGANContrastive, TSGANTriplet
+
+if __name__ == '__main__':
+    # Loads and tokenizes the data
+    data = l.load_data('coco_image_captions')
+    tokens = l.tokenize_data(data)
+
+    # Creates the sentence-based corpus
+    corpus = SentenceCorpus(tokens=tokens, min_frequency=1, max_pad_length=10)
+
+    # Initializes, learns the encoder and encodes the data
+    encoder = IntegerEncoder()
+    encoder.learn(corpus.vocab_index, corpus.index_vocab)
+    encoded_tokens = encoder.encode(corpus.tokens)
+
+    # Splits the tokens
+    enc_train, enc_val, enc_test = l.split_data(encoded_tokens, 0.8, 0.1, 0.1, 0)
+
+    # Creates Language Modeling datasets
+    train = LanguageModelingDataset(enc_train, batch_size=4)
+    val = LanguageModelingDataset(enc_val, batch_size=4)
+
+    #
+    model = TSGANContrastive(encoder=encoder, vocab_size=corpus.vocab_size,
+                            embedding_size=64, hidden_size=128)
+
+    # model = TSGANTriplet(encoder=encoder, vocab_size=corpus.vocab_size,
+    #                      embedding_size=64, hidden_size=128)
+
+    # Compiles the model
+    model.compile(pre_d_optimizer=tf.optimizers.Adam(learning_rate=0.001),
+                pre_g_optimizer=tf.optimizers.Adam(learning_rate=0.001),
+                d_optimizer=tf.optimizers.Adam(learning_rate=0.001),
+                g_optimizer=tf.optimizers.Adam(learning_rate=0.001))
+
+    #
+    model.pre_fit(train.batches, g_epochs=0, d_epochs=100)
