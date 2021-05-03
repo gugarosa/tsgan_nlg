a884d10ecfe509fd5b79c552e0826737e7c59465
Gustavo Rosa <gth.rosa@uol.com.br>
2021-05-03 10:23:48 -0300

    Fixing up language generation during optimization.

--- src/optimization/target.py
+++ src/optimization/target.py
@@ -125,10 +125,10 @@ def fine_tune_tsgan(model_name, model_obj, train, val_tokens, encoder, vocab_siz
             start_token = decoded_token[:n_tokens]
 
             # Generates with three distinct strategies
-            greedy_token = model.generate_greedy_search(start=start_token, max_length=len(decoded_token)-n_tokens)
-            temp_token = model.generate_temperature_sampling(start=start_token, max_length=len(decoded_token)-n_tokens,
+            greedy_token = model.G.generate_greedy_search(start=start_token, max_length=len(decoded_token)-n_tokens)
+            temp_token = model.G.generate_temperature_sampling(start=start_token, max_length=len(decoded_token)-n_tokens,
                                                              temperature=temp)
-            top_token = model.generate_top_sampling(start=start_token, max_length=len(decoded_token)-n_tokens,
+            top_token = model.G.generate_top_sampling(start=start_token, max_length=len(decoded_token)-n_tokens,
                                                     k=top_k, p=top_p)
 
             # Appends the outputs to lists
