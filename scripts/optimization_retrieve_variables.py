d9f4eeacd41b54445aab1289e114e2a63ec04a3d
Gustavo Rosa <gth.rosa@uol.com.br>
2022-01-14 16:02:26 -0300

    chore(scripts): Adds script that display the best optimization variables.

---
+++ scripts/optimization_retrieve_variables.py
@@ -0,0 +1,80 @@
+# Caveat to allow running from inside or outside scripts package
+import os
+import sys
+
+sys.path.append(os.path.abspath('./src'))
+sys.path.append(os.path.abspath('../src'))
+
+import argparse
+import glob
+import pickle
+
+import numpy as np
+from natsort import natsorted
+from opytimizer.utils.history import History
+
+import utils.plotter as p
+
+
+def load_history_wrapper(input_path):
+    """Wraps optimization history loading into a single method.
+
+    Args:
+        input_path (str): File to be loaded.
+
+    Returns:
+        An optimization history object.
+
+    """
+
+    # Loads object from file
+    with open(input_path, "rb") as input_file:
+        h = pickle.load(input_file)
+
+    return h
+
+
+def get_arguments():
+    """Gets arguments from the command line.
+
+    Returns:
+        A parser with the input arguments.
+
+    """
+
+    parser = argparse.ArgumentParser(usage='Retrieves the best variables from sets of .pkl optimization history files.')
+
+    parser.add_argument('history_files_stem', help='Input .pkl optimization history files without extension and seed',
+                        type=str, nargs='+')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    # Gathers the input parsed arguments
+    args = get_arguments()
+
+    # Common-based arguments
+    history_files_stem = args.history_files_stem
+
+    # Checks the folder and creates list of lists of input files
+    list_history_files = [natsorted(glob.glob(f'{history_file_stem}*'))
+                          for history_file_stem in history_files_stem]
+
+    # Loads sets of optimization history files
+    histories = [[load_history_wrapper(history_file) for history_file in history_files]
+                for history_files in list_history_files]
+
+    # Gathers best agent's variables
+    # Note that we will perform this for every set of input history files
+    best_agent_vars_per_input = [[h.get_convergence('best_agent')[0][:,-1] for h in history]
+                                 for history in histories]
+
+    # Peforms a mean calculation over their variables
+    mean_best_agent_vars_per_input = [np.mean(var, axis=0) for var in best_agent_vars_per_input]
+
+    # Displays the best variables
+    for mean_best_agent_vars in mean_best_agent_vars_per_input:
+        print(f'g_lr: {mean_best_agent_vars[0]} | d_lr: {mean_best_agent_vars[1]} | ' +
+              f'embedding_size: {int(mean_best_agent_vars[2])} | hidden_size: {int(mean_best_agent_vars[3])} | ' +
+              f'tau: {int(mean_best_agent_vars[4])}\n')
